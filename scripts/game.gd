ff027fab5efdd3db073632b0a7b5ae8df164a135
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2025-12-22 12:32:51 -0500

    refactor: center scenes without needing script

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,18 +2,3 @@ extends Node2D
 
 @onready var maze := $Maze
 @onready var pellets := $Pellets
-
-func _ready():
-	_center_maze()
-
-func _center_maze():
-	var viewport_size: Vector2 = get_viewport().get_visible_rect().size / scale
-	var tile_size: Vector2 = Vector2(maze.tile_set.tile_size)
-
-	var used: Rect2i = maze.get_used_rect()
-	var maze_size: Vector2 = tile_size * Vector2(used.size)
-
-	var desired_top_left: Vector2 = (viewport_size - maze_size) / 2.0
-	var offset_from_origin: Vector2 = tile_size * Vector2(used.position)
-	maze.position = desired_top_left - offset_from_origin
-	pellets.position = maze.position
--- scripts/title.gd
+++ scripts/title.gd
@@ -2,22 +2,6 @@ extends Node2D
 
 const GAME_SCENE_PATH := "res://scenes/Game.tscn"
 
-@onready var text := $Text
-
-func _ready():
-	_center_maze()
-
-func _center_maze():
-	var viewport_size: Vector2 = get_viewport().get_visible_rect().size / scale
-	var tile_size: Vector2 = Vector2(text.tile_set.tile_size)
-
-	var used: Rect2i = text.get_used_rect()
-	var maze_size: Vector2 = tile_size * Vector2(used.size)
-
-	var desired_top_left: Vector2 = (viewport_size - maze_size) / 2.0
-	var offset_from_origin: Vector2 = tile_size * Vector2(used.position)
-	text.position = desired_top_left - offset_from_origin
-
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("start_game"):
 		get_tree().change_scene_to_file(GAME_SCENE_PATH)
