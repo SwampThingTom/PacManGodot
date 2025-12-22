29f22cda70b83084d230f12aa89183bd2f9d97a2
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2025-12-22 16:53:56 -0500

    chore: replace tabs with spaces

--- scripts/title.gd
+++ scripts/title.gd
@@ -3,5 +3,5 @@ extends Node2D
 const GAME_SCENE_PATH := "res://scenes/Game.tscn"
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("start_game"):
-		get_tree().change_scene_to_file(GAME_SCENE_PATH)
+    if event.is_action_pressed("start_game"):
+        get_tree().change_scene_to_file(GAME_SCENE_PATH)
