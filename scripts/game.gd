ab1ecec5956820a349aa028425266a6b9a2b3793
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2025-12-22 16:56:40 -0500

    feat: show ready screen and spawn Pac-Man

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,4 +1,33 @@
 extends Node2D
 
+@export var pacman_scene: PackedScene
+
 @onready var maze := $Maze
 @onready var pellets := $Pellets
+@onready var ready_text := $ReadyText
+@onready var actors := $Actors
+
+const SPAWN_DURATION := 3.0 # Seconds to wait before spawning Pac-Man & ghosts
+const READY_DURATION := 1.0 # Seconds after spawning before start of game
+
+var pacman: Node2D
+
+func _ready() -> void:
+    run_intro()
+
+func run_intro() -> void:
+    ready_text.visible = true
+    await get_tree().create_timer(SPAWN_DURATION).timeout
+    spawn_actors()
+    await get_tree().create_timer(READY_DURATION).timeout
+    ready_text.visible = false
+
+func spawn_actors() -> void:
+    pacman = pacman_scene.instantiate()
+    pacman.global_position = pacman_start_position()
+    actors.add_child(pacman)
+
+func pacman_start_position() -> Vector2:
+    var p1: Vector2 = maze.map_to_local(Vector2i(13, 27))
+    var p2: Vector2 = maze.map_to_local(Vector2i(14, 27))
+    return (p1 + p2) * 0.5
