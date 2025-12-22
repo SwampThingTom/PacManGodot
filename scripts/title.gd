086df273ca427a5672b7a18b151aa1fabeae2198
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2025-12-22 18:25:49 -0500

    refactor: fix case of scene filename

--- scripts/title.gd
+++ scripts/title.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const GAME_SCENE_PATH := "res://scenes/Game.tscn"
+const GAME_SCENE_PATH := "res://scenes/game.tscn"
 
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("start_game"):
