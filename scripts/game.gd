6020abf2365e296dcb5eb7e8556e36e1d98f835a
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2025-12-29 07:38:28 -0500

    refactor: create states for round phases

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,11 +5,13 @@ extends Node2D
 ## Tracks number of players, current player, scores, and level.
 
 enum State {
-    NOT_PLAYING,
-    GET_READY,
+    START_GAME,
+    START_LEVEL,
+    START_ROUND,
     PLAYING,
-    DYING,
-    GAME_OVER
+    PLAYER_DIED,
+    LEVEL_COMPLETE,
+    GAME_OVER,
 }
 
 const BLINKY_FRAMES := preload("res://resources/blinky.tres")
@@ -22,7 +24,7 @@ const CLYDE_FRAMES  := preload("res://resources/clyde.tres")
 @export var spawn_duration_sec: float = 3.0 # Seconds to wait before spawning Pac-Man & ghosts
 @export var ready_duration_sec: float = 1.0 # Seconds after spawning before start of game
 
-var _state: State = State.NOT_PLAYING
+var _state: State = State.START_GAME
 var _level: int = 1
 var _current_player: int = 0
 var _high_score: int = 0
@@ -38,11 +40,8 @@ var _pacman: PacMan
 
 
 func _ready() -> void:
-    _reset_scores()
-    _spawn_actors()
-    _connect_signals()
-    _level_start()
-    
+    _start_game()
+
 
 func _process(delta: float) -> void:
     if _state != State.PLAYING:
@@ -64,14 +63,18 @@ func _transition_to(new_state: State) -> void:
 
 func _enter_state(s: State) -> void:
     match s:
-        State.GET_READY:
-            _enter_get_ready()
+        State.START_LEVEL:
+            _start_level()
+        State.START_ROUND:
+            _start_round()
         State.PLAYING:
-            _enter_playing()
-        State.DYING:
-            _enter_dying()
+            _playing()
+        State.PLAYER_DIED:
+            _player_died()
+        State.LEVEL_COMPLETE:
+            _level_complete()
         State.GAME_OVER:
-            _enter_game_over()
+            _game_over()
 
 
 # TODO: is this needed?
@@ -79,73 +82,71 @@ func _exit_state(s: State) -> void:
     pass
 
 
-func _enter_get_ready() -> void:
-    print("Starting GET_READY state")
-    _round_prepare()
-    await get_tree().create_timer(spawn_duration_sec).timeout
-    _show_actors()
-    await get_tree().create_timer(ready_duration_sec).timeout
-    ready_text.hide()
+func _start_game() -> void:
+    # Initialize game.
+    _spawn_actors()
+    _connect_signals()
+    _reset_scores()
+    _transition_to(State.START_LEVEL)
+
+
+func _start_level() -> void:
+    # Level-specific resets should all happen here.
+    # Call `_on_level_start()` for each subsystem.
+    print("START_LEVEL ", _level)
+    ghosts.reset_to_level(_level)
+    _transition_to(State.START_ROUND)
+
+
+func _start_round() -> void:
+    # Round-specific resets should all happen here.
+    # Restore positions / state without starting game.
+    print("START_ROUND")
+    _reset_spawn_positions()
+    await _show_ready_sequence()
     _transition_to(State.PLAYING)
 
 
-func _enter_playing() -> void:
-    print("Starting PLAYING state")
-    _round_start()
-
-
-func _enter_dying() -> void:
-    print("Starting DYING state")
-    _round_end()
-
-
-func _enter_game_over() -> void:
-    print("Starting GAME_OVER state")
-
-
-# -----------------------------------------------
-# Event Hooks
-# -----------------------------------------------
-
-func _start_round() -> void:
-    _round_prepare()
-    _transition_to(State.GET_READY)
-
-
-func _level_start() -> void:
-    # Level-specific resets should all happen here.
-    # Call `_on_level_start()` for each subsystem.
-    ghosts.reset_to_level(_level)
-    ghost_mode.stop()
-    _start_round()
-
-
-func _round_prepare() -> void:
-    # Round-specific resets should all happen here.
-    # Restore positions / state without starting game.
-    _reset_spawn_positions()
-    ready_text.show()
-
-
-func _round_start() -> void:
+func _playing() -> void:
     # Start playing the next round (timers, counters, movement, etc.)
+    print("PLAYING")
     _pacman.start_moving()
     ghosts.start_round()
     ghost_mode.start(_level)
 
 
-func _round_end() -> void:
+func _player_died() -> void:
     # Stop playing (timers, counters, movement, etc.)
     # Show death animation
+    print("PLAYER_DIED")
     _stop_actors()
     ghosts.on_life_lost()
     await _run_death_sequence()
     _start_round() # TODO: Only if game not over
 
 
+func _level_complete() -> void:
+    # Stop playing (timers, counters, movement, etc.)
+    # Start next level
+    print("LEVEL_COMPLETE")
+    _stop_actors()
+
+
+func _game_over() -> void:
+    print("GAME_OVER")
+
+
 # -----------------------------------------------
-# Death Sequence
+# Transition sequences
 # -----------------------------------------------
+
+func _show_ready_sequence() -> void:
+    ready_text.show()
+    await get_tree().create_timer(spawn_duration_sec).timeout
+    _show_actors()
+    await get_tree().create_timer(ready_duration_sec).timeout
+    ready_text.hide()
+
 
 func _run_death_sequence() -> void:
     await get_tree().create_timer(1.0).timeout
@@ -167,13 +168,13 @@ func _check_collisions() -> void:
         if ghost.state != Ghost.State.ACTIVE:
             continue
         if ghost.get_cell() == pacman_cell:
-            _handle_collision(ghost)
+            _on_collision(ghost)
             return
 
 
-func _handle_collision(ghost: Ghost) -> void:
+func _on_collision(ghost: Ghost) -> void:
     if ghost_mode.get_mode() != GhostMode.Mode.FRIGHTENED:
-        _transition_to(State.DYING)
+        _transition_to(State.PLAYER_DIED)
 
 
 # -----------------------------------------------
@@ -187,9 +188,7 @@ func _on_pellet_eaten(is_power_pellet: bool):
 
 
 func _on_all_pellets_eaten():
-    print("Level Complete!")
-    _state = State.GAME_OVER
-    _stop_actors()
+    _transition_to(State.LEVEL_COMPLETE)
 
 
 # -----------------------------------------------
