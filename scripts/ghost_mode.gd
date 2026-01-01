d5589cd6215d6d9afd08630bea93f3ad2e8c586b
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2026-01-01 10:32:30 -0500

    refactor: make the ghost mode durations const

--- scripts/ghost_mode.gd
+++ scripts/ghost_mode.gd
@@ -11,11 +11,15 @@ enum Mode { SCATTER, CHASE }
 # Time spent in a single color (blue or white) while flashing.
 const FLASH_COLOR_SECONDS: float = 8.0 / 60.0
 
-static var _mode_durations: Array[Array] = []
+const MODE_DURATIONS: Array[Array] = [
+    [7.0, 20.0, 7.0, 20.0, 5.0,   20.0, 5.0],    # Level 1
+    [7.0, 20.0, 7.0, 20.0, 5.0, 1033.0, 0.016],  # Level 2-4
+    [5.0, 20.0, 5.0, 20.0, 5.0, 1037.0, 0.016],  # Level 5+
+]
 
 var _running := false
 var _level: int = 0
-var _level_index: int = 0
+var _duration_index: int = 0
 var _mode_index: int = 0
 var _mode = Mode.SCATTER
 var _duration: float = 0.0
@@ -23,16 +27,6 @@ var _is_frightened = false
 var _frightened_duration: float = 0.0
 var _next_flash_time: float
 var _next_flash_is_white: bool
-
-
-static func _static_init() -> void:
-    # Level 1
-    var durations_1: Array[float] = [7.0, 20.0, 7.0, 20.0, 5.0,   20.0, 5.0]
-    # Level 2-4
-    var durations_2: Array[float] = [7.0, 20.0, 7.0, 20.0, 5.0, 1033.0, 0.016]
-    # Level 5+
-    var durations_3: Array[float] = [5.0, 20.0, 5.0, 20.0, 5.0, 1037.0, 0.016]
-    _mode_durations = [durations_1, durations_2, durations_3]
 
 
 func _process(delta: float) -> void:
@@ -43,7 +37,7 @@ func _process(delta: float) -> void:
         _process_frightened_mode(delta)
         return
     
-    if _mode_index >= _mode_durations[0].size():
+    if _mode_index >= MODE_DURATIONS[0].size():
         return
     
     _duration -= delta
@@ -60,7 +54,7 @@ func _process(delta: float) -> void:
 
 func on_start_level(level: int) -> void:
     _level = level
-    _level_index = _get_level_index(_level)
+    _duration_index = _get_duration_index(_level)
 
 
 func on_start_round() -> void:
@@ -100,12 +94,13 @@ func start_frightened() -> void:
 # -----------------------------------------------
 
 func _get_duration() -> float:
-    if _mode_index >= _mode_durations[0].size():
+    if _mode_index >= MODE_DURATIONS[0].size():
         return 0.0
-    return _mode_durations[_level_index][_mode_index]
+    return MODE_DURATIONS[_duration_index][_mode_index]
 
 
-func _get_level_index(level: int) -> int:
+# Used for accessing the MODE_DURATIONS array.
+func _get_duration_index(level: int) -> int:
     if level == 1:
         return 0
     if level < 5:
